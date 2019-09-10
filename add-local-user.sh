b148d1b3bed029eacaad05bb2328aaba5cfff02f
sradms0 <sradms0@openmailbox.org>
2019-09-10 12:51:02 -0400

    add random password generation and allow user to be added with args

--- add-local-user.sh
+++ add-local-user.sh
@@ -9,25 +9,29 @@ then
     exit 1
 fi
 
-echo '*** Create a new user ****'
+# Inform the user the correct argument length (2) was not provided.
+if [[ "${#}" -ne 2 ]]
+then
+    echo "Usage: ${0} [USER_NAME] [ACCOUNT_HOLDER]"
+    exit 1
+fi
 
-# Prompt for username to create.
-read -p 'Enter username: ' USERNAME
+# Store username and comment (account holder).
+USERNAME="${1}"
+COMMENT="${2}"
 
-# Prompt for the name for person who will be using the account.
-read -p 'Enter account holder name: ' COMMENT
-
-# Prompt for the initial password for the account.
-read -p 'Enter password: ' PASSWORD
+# Generate a random password.
+SYMBOLS='$%^&*()_-+=@'
+SHUFFLED_SYMBOL=$( echo "${SYMBOLS}" | fold -w1 | shuf | head -c1 )
+SHUFFLED_DATE=$( echo $(date +%s%N | fold -w1 | shuf) | tr -d ' ' | head -c5)
+RANDOM_NUM=$(echo "${RANDOM}" | head -c1)
+PASSWORD="${RANDOM_NUM}${SHUFFLED_SYMBOL}${SHUFFLED_DATE}"
 
 # Create the user.
 useradd -c "${COMMENT}" -m "${USERNAME}"
 
-# Set the password for the user
+# Set the password for the user.
 echo "${USERNAME}:${PASSWORD}" | chpasswd
-
-# Force password change on first login
-passwd -e ${USERNAME}
 
 # Inform the user if the account was not able to be created for some reason. 
 if [[ "${?}" -ne 0 ]]
@@ -36,6 +40,6 @@ then
     exit 1
 fi
 
-# Display the username, password, and host where the account was created. 
+## Display the username, password, and host where the account was created. 
 echo "*** User successfully created at $(hostname)***"
 echo "username: ${USERNAME} password: ${PASSWORD}"
